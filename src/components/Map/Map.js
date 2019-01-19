4c0c81e9f0dbcf044d870fae2c2601f526a0a105
Snake911 <snake1234567890-1@yandex.ru>
2019-01-19 13:55:15 +0500

    add marker map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,17 +2,47 @@ import React from 'react';
 import classes from './Map.css';
 import { Map, Marker } from 'yandex-map-react';
 
-export default class ContactMap extends React.Component{
+class ContactMap extends React.Component{
+    state = {
+        coordinates:[57.91944, 59.965],
+        userCoordLat:57.91944, 
+        userCoordLon: 59.965
+    }
+    coords = () => {
+        navigator.geolocation.getCurrentPosition ( async (position) => {
+            this.setState({
+                userCoordLat: position.coords.latitude,
+                userCoordLon: position.coords.longitude
+            })
+        })     
+    }
+    componentWillMount(){
+        this.coords();
+    }
+
+    geocode(ymaps) {
+        ymaps.geocode('Нижний Тагил, пр. Уральский, 58')
+          .then(result => this.setState({ coordinates: result.geoObjects.get(0).geometry.getCoordinates() }))
+    }
     render () {
-        const lat = +this.props.lat;
-        const lon = +this.props.lon;
-        return (
-            
-            <div className={classes.Map}>
-                <Map onAPIAvailable={function () { console.log('API loaded'); }} center={[lat, lon]} zoom={10}>
-                    <Marker lat={lat} lon={lon} />
+        const loadOptions = {
+            lang: 'ru_RU',
+            apikey: '424e8338-02d6-4172-8698-59b1cd6343d4'            
+        }    
+        return (            
+            <div className={classes.Map}>                        
+                <Map 
+                    loadOptions={loadOptions} 
+                    onAPIAvailable={(ymaps) => this.geocode(ymaps)} 
+                    center={[this.state.coordinates[0], this.state.coordinates[1]]}
+                    zoom={14}>
+                    <Marker lat={this.state.userCoordLat} lon={this.state.userCoordLon}/>                     
                 </Map>
+                {console.log(this.state)}           
             </div>
+            
         );
     }
 }
+
+export default ContactMap;
